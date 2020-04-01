57f780dced5b1e52d6eeb18e8ef9f49b1caaaae9
Gabriel Antiqueira <gmantiqueira@outlook.com>
2020-03-31 23:31:49 -0300

    Numeric keyboard

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -31,6 +31,7 @@ export default function SignIn() {
         onSubmitEditing={handleLogin}
         value={id}
         onChangeText={setId}
+        keyboardType="numeric"
       />
       <Button
         loading={loading}
